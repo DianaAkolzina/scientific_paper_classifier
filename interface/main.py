1e79fc96ea72365a84c53d9f7e299ac606a01480
Diana Akolzina <dakolzina@gmail.com>
2024-05-09 09:55:21 +0100

    updates to main

--- interface/main.py
+++ interface/main.py
@@ -1,33 +1,59 @@
 import os
+import pandas as pd
 from data.getdata import get_data_from_gcp
-from models.baseline_model import vectorize_data, initialize_model, train_svm_model, evaluate_model
-from models.preprocess import preprocessing_pipeline
+from models.baseline_model import initialize_model, train_svm_model, evaluate_model
+from models.preprocess import preprocessing_pipeline, preprocessing_pipeline_sample
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
 
-#Pull the data
+# Pull the data
 BUCKET_NAME = os.getenv("BUCKET_NAME")
-filename = ''
-
+filename = 'cleaned_data/Updated_df_30000.csv'
 df = get_data_from_gcp(BUCKET_NAME, filename)
 
-#Preprocess the data
-text_column = df['Main Body']
-author_column = df['Author']
-label_column = df['Label']
-
+# Preprocess the data
+text_column = 'Processed Text'
+author_column = 'Author'
+label_column = 'Label'
 preprocessed_data = preprocessing_pipeline(df, text_column, author_column, label_column)
 
-#Split data into features and target
-X = preprocessed_data['Processed Text']
-y = preprocessed_data['Label']
+# Reset DataFrame and type labels
+combined_df = preprocessed_data
+combined_df.reset_index(drop=True, inplace=True)
+combined_df['Label'] = combined_df['Label'].astype('category')
 
-#Vectorize the data
-X_tfidf = vectorize_data(X)
+# Balance the dataset
+label_counts = combined_df['Label'].value_counts()
+min_category_size = label_counts.min()
+balanced_df = pd.concat([
+    combined_df[combined_df['Label'] == label].sample(n=min_category_size, random_state=42)
+    for label in combined_df['Label'].cat.categories
+])
+balanced_df = balanced_df.sample(frac=1, random_state=42).reset_index(drop=True)
 
-#Train, test, split
+X = balanced_df['Processed Text']
+y = balanced_df['Label']
+
+# Initialize and fit the TF-IDF Vectorizer
+tfidf = TfidfVectorizer()
+X_tfidf = tfidf.fit_transform(X)
+
+# Train, test, split
 X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2, random_state=42)
 
-#Initialize, train and evaluate a model
+# Initialize, train and evaluate the SVM model
 svm_model = initialize_model()
 trained_model = train_svm_model(svm_model, X_train, y_train)
 evaluate_model(trained_model, X_test, y_test)
+
+file_path = 'data/sample_scientific/your_file.txt'
+
+# Open the text file and read its contents
+with open(file_path, 'r', encoding='utf-8') as file:
+    new_text = file.read()
+
+processed_new_text = preprocessing_pipeline_sample(new_text)
+new_text_tfidf = tfidf.transform([processed_new_text])
+predicted_label = trained_model.predict(new_text_tfidf)
+print("Predicted Label:", predicted_label[0])
